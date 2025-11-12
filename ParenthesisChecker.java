d17a4eb1313db625675348133aa0072f7b2fb945
imredshadow <jianturalbaacol@gmail.com>
2025-11-12 18:51:15 +0000

    41

--- ParenthesisChecker.java
+++ ParenthesisChecker.java
@@ -6,19 +6,23 @@ public class ParenthesisChecker{
     
 
     public static void main(String[] args){
-        
+        //Makes a scanner and awaits the next line of input to check
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a string of brackets: ");
         String input = scanner.nextLine();
-
+        
+        //Precondition: Must have an string input before running
+        //Postcondition: will remove every instance of two together brackets
         while (input.contains("()") || input.contains("{}") || input.contains("[]")) {
             input = input.replace("()", "");
             input = input.replace("{}", "");
             input = input.replace("[]", "");
         }
 
+        //Checks if there are leftover brackets or not
         boolean valid = input.isEmpty();
 
+        //Print true or false based on if it is empty or not
         if (valid == true) {
             System.out.println("Valid sentence!");
         } else {
